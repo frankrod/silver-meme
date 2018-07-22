63e84e1440beafe9c7c81d7aea21c79212c5329e
Frank <frankhn0801@gmail.com>
2018-07-22 14:59:33 -0600

    added hability to use redux dev tools

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 
 import rootReducer from './rootReducer'
@@ -6,10 +6,26 @@ import sagas from './sagas'
 
 export default function configureStore() {
   const sagaMiddleware = createSagaMiddleware()
+  const enhancers = [];
+  const initialState = {};
+
+  if (process.env.NODE_ENV === 'development') {
+    const devToolsExtension = window.devToolsExtension;
+  
+    if (typeof devToolsExtension === 'function') {
+      enhancers.push(devToolsExtension());
+    }
+  }
+
+  const composedEnhancers = compose(
+    applyMiddleware(sagaMiddleware),
+    ...enhancers
+  );
 
   const store = createStore(
     rootReducer,
-    applyMiddleware(sagaMiddleware)
+    initialState,
+    composedEnhancers
   )
 
   sagaMiddleware.run(sagas);
